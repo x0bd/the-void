67e4e08f8e1fb6cd4eaa180fefbf4b82c4bb99a3
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-19 12:17:09 +0200

    remove lottie animations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import React from "react";
 import { Icon } from "@iconify/react";
-import Image from "next/image";
 import { Player } from "@lottiefiles/react-lottie-player";
 
 const Footer = () => {
@@ -11,19 +10,9 @@ const Footer = () => {
 		<div className="flex flex-col gap-5">
 			<div className="bg-neutral-600  w-full h-[1px]"></div>
 			<div className="flex justify-between items-center">
-				<div className="flex justify-between items-center">
-					<Player
-						loop
-						autoplay
-						style={{ height: "50px", width: "50px" }}
-						src="https://lottie.host/97b7b3fe-d53b-48ae-b318-af6146299f41/zybAwdKNeG.json"
-						// src="https://lottie.host/910279fc-fdbb-4f2c-9a93-be0f29e17552/JIR8slGAFS.json"
-					/>
-					<h1 className="text-sm text-neutral-500 font-mono">
-						signals from the void
-					</h1>
-				</div>
-
+				<h1 className="text-sm text-neutral-500 font-mono">
+					signals from the void
+				</h1>
 				<div className="flex justify-evenly gap-4">
 					<Link href="/" target="_blank">
 						<Icon icon="pajamas:twitter" />
