72f6c5d18dae67adba3369e2715e8ee3ecfd578f
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-19 13:19:18 +0200

    wip cache settings

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,7 @@
 import Posts from "@/components/Posts";
 import { Metadata } from "next";
 
-export const revalidate = 10;
+export const revalidate = 3600;
 
 export const metadata: Metadata = {
 	title: "notes and doodles",
--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -2,7 +2,7 @@ import { getPostsMeta } from "@/lib/posts";
 import ListItem from "@/components/ListItem";
 import Link from "next/link";
 
-export const revalidate = 10;
+export const revalidate = 3600;
 
 type Props = {
 	params: {
