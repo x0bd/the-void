5fc2272034be72336ef8eb5af853b701bfa967de
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-20 23:50:48 +0200

    wip art gallery

--- app/art/page.tsx
+++ app/art/page.tsx
@@ -11,15 +11,19 @@ const Art = () => {
 	return (
 		<main>
 			<h1>Art, doodles, generative</h1>
-			<div className="px-2 my-3 grid gap-2 grid-cols-gallery">
+			<div className="px-2 my-3 grid gap-4 grid-cols-gallery auto-rows-[10px]">
 				{images.map((image, index) => (
-					<div key={index}>
+					<div
+						className="relative overflow-hidden h-40 mb-4 group"
+						key={index}
+					>
 						<Image
 							src={image.src}
 							alt={image.alt}
-							width={image.width}
-							height={image.height}
-							className="rounded-lg"
+							fill={true}
+							priority
+							sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+							className="rounded-lg object-cover hover:opacity-75"
 						/>
 					</div>
 				))}
