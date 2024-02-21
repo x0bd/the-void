bef5fb9e456e4aa77e242ddeb2950c12b221c7be
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-21 17:52:35 +0200

    wip music page done, api routes to app was brutal

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import { getTopTracks, getTopArtists } from "@/lib/spotify";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default page
+// Assuming the functions getTopTracks and getTopArtists are async and return promises
+const MusicPage = () => {
+	const [tracks, setTracks] = useState([]);
+	const [artists, setArtists] = useState([]);
+
+	useEffect(() => {
+		const fetchTracksAndArtists = async () => {
+			const tracksData = await getTopTracks();
+			const artistsData = await getTopArtists();
+			setTracks(tracksData);
+			setArtists(artistsData);
+		};
+
+		fetchTracksAndArtists();
+	}, []);
+
+	return (
+		<div>
+			<h1 className="text-neutral-100 text-2xl font-semibold my-8">
+				Top Tracks
+			</h1>
+
+			<div className="flex flex-col gap-y-4">
+				{tracks.map(({ artists, album, songUrl, title, image }) => (
+					<div
+						key={album}
+						className="flex flex-row items-center space-x-4"
+					>
+						<img src={image} alt={title} className="w-16 h-16" />
+						<div className="flex flex-col">
+							<Link href={songUrl}>
+								<h3 className="font-semibold text-neutral-100">
+									{title}
+								</h3>
+							</Link>
+							<p>{artists.map(({ name }) => name).join(", ")}</p>
+							<p>{album}</p>
+						</div>
+					</div>
+				))}
+			</div>
+
+			<h1 className="text-neutral-100 text-2xl font-semibold my-8">
+				Top Artists
+			</h1>
+
+			<div className="flex flex-col gap-y-4">
+				{artists.map(({ name, url, image, followers }) => (
+					<div
+						key={name}
+						className="flex flex-row items-center space-x-4"
+					>
+						<img src={image} alt={name} className="w-16 h-16" />
+						<div className="flex flex-col">
+							<Link href={url}>
+								<h3 className="font-semibold text-neutral-100">
+									{name}
+								</h3>
+							</Link>
+							<p>{followers} followers</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default MusicPage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true,
+
+	env: {
+		SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
+		SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+		SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+	},
+
 	images: {
 		remotePatterns: [
 			{
