e03583cd0cca0e64f2efe85dbc56c4cd8886998d
Tinodaishe Tembo <synthmancer@gmail.com>
2024-03-02 08:19:08 +0200

    minor ui fix

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -57,7 +57,7 @@ export default function Page() {
 								alt={title}
 								width={100}
 								height={100}
-								className="w-20 h-20"
+								className="w-20 rounded-lg h-20"
 							/>
 							<div className="flex flex-col">
 								<Link href={songUrl}>
@@ -91,7 +91,7 @@ export default function Page() {
 								alt={artist.name}
 								height={100}
 								width={100}
-								className="w-20 h-20"
+								className="w-20 rounded-lg h-20"
 							/>
 							<div className="flex flex-col">
 								<h3 className="fomt-semibold text-neutral-100">
