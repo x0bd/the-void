9c3ec932e2778cbe27d5eb5efbe28531ac08a08f
Tinodaishe Tembo <synthmancer@gmail.com>
2024-03-23 10:51:23 +0200

    add 3body experiment

--- data/experiment.ts
+++ data/experiment.ts
@@ -8,6 +8,13 @@ export type Experiment = {
 
 export const images: Experiment[] = [
 	{
+		src: "/images/3body.gif",
+		alt: "3 body problem simulator",
+		width: 800,
+		height: 800,
+		link: "/",
+	},
+	{
 		src: "/images/joy.png",
 		alt: "unknown pleasures",
 		width: 700,
@@ -15,17 +22,17 @@ export const images: Experiment[] = [
 		link: "/",
 	},
 	{
+		src: "/images/morty.png",
+		alt: "Ascii image of Morty Smith",
+		width: 1040,
+		height: 1005,
+		link: "https://github.com/x0bd/rascii",
+	},
+	{
 		src: "/images/waves.png",
 		alt: "hard waves",
 		width: 700,
 		height: 700,
 		link: "/",
-	},
-	{
-		src: "/images/morty.png",
-		alt: "Morty Smith",
-		width: 1040,
-		height: 1005,
-		link: "https://github.com/x0bd/rascii",
 	},
 ];
